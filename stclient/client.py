df92e096ad368e8214e4b406995257820d26f669
Jan Koritak <jan.koritak@kiwi.com>
2019-06-17 01:15:37 +0200

    Game Events handling, Conditional Wait, Proof of concept script

--- stclient/client.py
+++ stclient/client.py
@@ -1,5 +1,7 @@
 import attr
 from datetime import datetime
+from collections import defaultdict
+import json
 import logging
 import logging.config
 import socket
@@ -22,11 +24,38 @@ from pypb.command_deck_select_pb2 import (
 from pypb.command_draw_cards_pb2 import (
     Command_DrawCards
 )
+from pypb.command_move_card_pb2 import (
+    Command_MoveCard
+)
 from pypb.command_ready_start_pb2 import (
     Command_ReadyStart
 )
+from pypb.command_set_active_phase_pb2 import (
+    Command_SetActivePhase
+)
+from pypb.command_next_turn_pb2 import (
+    Command_NextTurn
+)
+from pypb.command_set_card_attr_pb2 import (
+    Command_SetCardAttr
+)
 from pypb.event_game_joined_pb2 import (
     Event_GameJoined
+)
+from pypb.event_game_state_changed_pb2 import (
+    Event_GameStateChanged
+)
+from pypb.event_set_active_phase_pb2 import (
+    Event_SetActivePhase
+)
+from pypb.event_set_active_player_pb2 import (
+    Event_SetActivePlayer
+)
+from pypb.event_draw_cards_pb2 import (
+    Event_DrawCards
+)
+from pypb.card_attributes_pb2 import (
+    CardAttribute
 )
 from pypb.commands_pb2 import CommandContainer
 from pypb.event_server_identification_pb2 import Event_ServerIdentification
@@ -65,6 +94,17 @@ class NoDataReceived(Exception):
 
 
 @attr.s
+class Card:
+    id = attr.ib()
+    name = attr.ib()
+    tapped = attr.ib(False)
+
+    @property
+    def info(self):
+        return DECK[self.name]
+
+
+@attr.s
 class Client:
 
     host = attr.ib()
@@ -75,6 +115,14 @@ class Client:
     cmd_counter = attr.ib(0, init=False)
 
     game_info = attr.ib(None, init=False)
+    game_started = attr.ib(False, init=False)
+    player_id = attr.ib(None, init=False)
+
+    active_phase = attr.ib(None, init=False)
+    active_player_id = attr.ib(None, init=False)
+
+    hand = attr.ib([], init=False)
+    table = attr.ib([], init=False)
 
     def connect(self):
         log.info("connect")
@@ -110,6 +158,9 @@ class Client:
 
             if server_message.message_type == ServerMessage.MessageType.SESSION_EVENT:
                 self.handle_session_event(server_message)
+
+            if server_message.message_type == ServerMessage.MessageType.GAME_EVENT_CONTAINER:
+                self.handle_game_event(server_message)
 
             if server_message.message_type != ServerMessage.MessageType.RESPONSE:
                 log.info("Ignoring message of type %s" % server_message.message_type)
@@ -162,8 +213,173 @@ class Client:
         if message.session_event.HasExtension(Event_GameJoined.ext):
             event_gamejoined = message.session_event.Extensions[Event_GameJoined.ext]
             self.game_info = event_gamejoined.game_info
+            self.player_id = event_gamejoined.player_id
             log.info("Entered game: %s" % event_gamejoined.game_info.game_id)
 
+    def handle_game_event(self, message):
+        for event in message.game_event_container.event_list:
+            log.info("Received game event")
+
+            if event.HasExtension(Event_GameStateChanged.ext):
+                event_gamestatechanged = event.Extensions[Event_GameStateChanged.ext]
+                self.game_started = event_gamestatechanged.game_started
+                log.info("Game has started")
+
+            elif event.HasExtension(Event_SetActivePhase.ext):
+                event_setactivephase = event.Extensions[Event_SetActivePhase.ext]
+                self.active_phase = event_setactivephase.phase
+                log.info("Active phase is %s" % self.active_phase)
+
+            elif event.HasExtension(Event_SetActivePlayer.ext):
+                event_setactiveplayer = event.Extensions[Event_SetActivePlayer.ext]
+                self.active_player_id = event_setactiveplayer.active_player_id
+                log.info("It's turn of player %s" % self.active_player_id)
+
+            elif event.HasExtension(Event_DrawCards.ext):
+                event_drawcards = event.Extensions[Event_DrawCards.ext]
+                for card in event_drawcards.cards:
+                    self.hand.append(Card(id=card.id, name=card.name))
+
+    def wait(self, check_condition):
+        log.info("Waiting on condition")
+        last_ping = datetime.now()
+        while not check_condition():
+            try:
+                client.recv()
+            except NoDataReceived:
+                pass
+
+            if datetime.now().timestamp() - last_ping.timestamp() > 4:
+                cmd_ping = Command_Ping()
+                client.send_command(cmd_ping, silent=True)
+                client.recv(silent=True)
+                last_ping = datetime.now()
+
+            time.sleep(0.5)
+
+
+def find_land(hand):
+    for card in hand:
+        if card.info["types"][0] == 'Land':
+            return card
+
+    return None
+
+def find_creatures(hand):
+    creatures = []
+    for card in hand:
+        if card.info["types"][0] == 'Creature':
+            creatures.append(card)
+
+    return creatures
+
+def find_lands_to_pay_for(card_to_play, table):
+    lands = []
+    lands_by_color = defaultdict(list)
+    selected_lands = []
+
+    for card in table:
+        if not card.tapped and card.info["types"][0] == 'Land':
+            lands.append(card)
+            for color in card.info["colorIdentity"]:
+                lands_by_color[color].append(card)
+
+    costs = card_to_play.info["manaCost"].strip("{}").split("}{")
+
+    # allocate particular color costs
+    for cost in costs:
+        if cost not in ["R", "W", "G", "U", "B"]:
+            continue
+
+        try:
+            selected_land = lands_by_color[cost].pop()
+        except IndexError:
+            return None
+
+        try:
+            selected_lands.append(selected_land)
+            lands.remove(selected_land)
+        except:
+            import pdb; pdb.set_trace()
+
+    # allocate dual color costs
+    for cost in costs:
+        if '/' not in cost:
+            continue
+
+        selected_land = None
+        for color in cost.split('/'):
+            try:
+                selected_land = lands_by_color[color].pop()
+            except IndexError:
+                pass
+
+        if selected_land is None:
+            return None
+
+        try:
+            selected_lands.append(selected_land)
+            lands.remove(selected_land)
+        except:
+            import pdb; pdb.set_trace()
+
+    # allocate color-less costs
+    for cost in costs:
+        if not cost.isnumeric():
+            continue
+
+        try:
+            for num in range(0, int(cost)):
+                selected_lands.append(lands.pop())
+        except IndexError:
+            return None
+
+    # If we reached here, we can afford to play the card :)
+    return selected_lands
+
+
+def play_card(card):
+    cmd_movecard = Command_MoveCard()
+    cmd_movecard.start_zone = "hand"
+    cmd_movecard.start_player_id = client.player_id
+    cmd_movecard.target_player_id = client.player_id
+    if card.info["types"][0] == 'Land':
+        cmd_movecard.target_zone = "table"
+        cmd_movecard.y = 2
+    else:
+        cmd_movecard.target_zone = "stack"
+    card_to_move = cmd_movecard.cards_to_move.card.add()
+    card_to_move.card_id = card.id
+    client.send_command(cmd_movecard)
+    client.recv()
+    client.hand.remove(card)
+    client.table.append(card)
+
+def tap_card(card, set_tapped=True):
+    cmd_setcardattr = Command_SetCardAttr()
+    cmd_setcardattr.attribute = CardAttribute.AttrTapped
+    cmd_setcardattr.attr_value = "1" if set_tapped else "0"
+    cmd_setcardattr.card_id = card.id
+    cmd_setcardattr.zone = "table"
+    client.send_command(cmd_setcardattr)
+    client.recv()
+    card.tapped = set_tapped
+
+    return
+
+
+    cmd_movecard = Command_MoveCard()
+    cmd_movecard.start_zone = "table"
+    cmd_movecard.target_zone = "table"
+    cmd_movecard.start_player_id = client.player_id
+    cmd_movecard.target_player_id = client.player_id
+    cmd_movecard.y = 2
+    card_to_move = cmd_movecard.cards_to_move.card.add()
+    card_to_move.card_id = card.id
+    card_to_move.tapped = set_tapped
+    client.send_command(cmd_movecard)
+    client.recv()
+    card.tapped = set_tapped
 
 if __name__ == "__main__":
     client = Client(host="127.0.0.1", port=4747)
@@ -211,15 +427,19 @@ if __name__ == "__main__":
 
     # Process GameJoined event
     cmd_ping = Command_Ping()
-    client.send_command(cmd_ping, silent=True)
-    client.recv(silent=True)
+    client.send_command(cmd_ping)
+    client.recv()
 
+    # Load Cockatrice deck
+    DECK_COCKATRICE = "<?xml version=\"1.0\"?><cockatrice_deck version=\"1\"><deckname></deckname><comments></comments><zone name=\"main\"><card number=\"1\" name=\"Lovisa Coldeyes\"/><card number=\"2\" name=\"Skarrgan Pit-Skulk\"/><card number=\"2\" name=\"Kruin Striker\"/><card number=\"1\" name=\"Talara\'s Battalion\"/><card number=\"1\" name=\"Radha, Heir to Keld\"/><card number=\"2\" name=\"Burning-Tree Emissary\"/><card number=\"1\" name=\"Zo-Zu the Punisher\"/><card number=\"1\" name=\"Relentless Hunter\"/><card number=\"2\" name=\"Ambassador Oak\"/><card number=\"2\" name=\"Gorehorn Minotaurs\"/><card number=\"2\" name=\"Cloudcrown Oak\"/><card number=\"1\" name=\"Rubblebelt Raiders\"/><card number=\"1\" name=\"Kamahl, Pit Fighter\"/><card number=\"1\" name=\"Boldwyr Intimidator\"/><card number=\"2\" name=\"Firebolt\"/><card number=\"2\" name=\"Rampant Growth\"/><card number=\"2\" name=\"Call of the Herd\"/><card number=\"1\" name=\"Harmonize\"/><card number=\"1\" name=\"Increasing Savagery\"/><card number=\"1\" name=\"Roar of the Wurm\"/><card number=\"1\" name=\"Guttural Response\"/><card number=\"2\" name=\"Sylvan Might\"/><card number=\"1\" name=\"Beacon of Destruction\"/><card number=\"1\" name=\"Beast Attack\"/><card number=\"1\" name=\"Coat of Arms\"/><card number=\"4\" name=\"Rugged Highlands\"/><card number=\"11\" name=\"Mountain\"/><card number=\"10\" name=\"Forest\"/></zone></cockatrice_deck>\n"
+    # Load deck json
+    with open("Might.json") as fp:
+        DECK_JSON = json.load(fp)
+    DECK = {card["name"]: card for card in DECK_JSON["mainBoard"]}
 
-    # Load deck
-    DECK = "<?xml version=\"1.0\"?><cockatrice_deck version=\"1\"><deckname></deckname><comments></comments><zone name=\"main\"><card number=\"1\" name=\"Lovisa Coldeyes\"/><card number=\"2\" name=\"Skarrgan Pit-Skulk\"/><card number=\"2\" name=\"Kruin Striker\"/><card number=\"1\" name=\"Talara\'s Battalion\"/><card number=\"1\" name=\"Radha, Heir to Keld\"/><card number=\"2\" name=\"Burning-Tree Emissary\"/><card number=\"1\" name=\"Zo-Zu the Punisher\"/><card number=\"1\" name=\"Relentless Hunter\"/><card number=\"2\" name=\"Ambassador Oak\"/><card number=\"2\" name=\"Gorehorn Minotaurs\"/><card number=\"2\" name=\"Cloudcrown Oak\"/><card number=\"1\" name=\"Rubblebelt Raiders\"/><card number=\"1\" name=\"Kamahl, Pit Fighter\"/><card number=\"1\" name=\"Boldwyr Intimidator\"/><card number=\"2\" name=\"Firebolt\"/><card number=\"2\" name=\"Rampant Growth\"/><card number=\"2\" name=\"Call of the Herd\"/><card number=\"1\" name=\"Harmonize\"/><card number=\"1\" name=\"Increasing Savagery\"/><card number=\"1\" name=\"Roar of the Wurm\"/><card number=\"1\" name=\"Guttural Response\"/><card number=\"2\" name=\"Sylvan Might\"/><card number=\"1\" name=\"Beacon of Destruction\"/><card number=\"1\" name=\"Beast Attack\"/><card number=\"1\" name=\"Coat of Arms\"/><card number=\"4\" name=\"Rugged Highlands\"/><card number=\"11\" name=\"Mountain\"/><card number=\"10\" name=\"Forest\"/></zone></cockatrice_deck>\n"
 
     cmd_deckselect = Command_DeckSelect()
-    cmd_deckselect.deck = DECK
+    cmd_deckselect.deck = DECK_COCKATRICE
     client.send_command(cmd_deckselect)
     client.recv()
 
@@ -229,24 +449,65 @@ if __name__ == "__main__":
     client.send_command(cmd_readystart)
     client.recv()
 
-    # Draw cards
+    # Wait for game to start
+    client.wait(lambda: client.game_started)
+
+    # Draw initial hand
     cmd_drawcards = Command_DrawCards()
-    cmd_drawcards.number = 10
+    cmd_drawcards.number = 7
     client.send_command(cmd_drawcards)
     client.recv()
 
-
-    last_ping = datetime.now()
     while True:
-        try:
-            client.recv()
-        except NoDataReceived:
-            pass
+        # Wait for player's turn
+        client.wait(lambda: client.active_player_id == client.player_id)
 
-        if datetime.now().timestamp() - last_ping.timestamp() > 1:
-            print("Sending ping")
-            cmd_ping = Command_Ping()
-            client.send_command(cmd_ping, silent=True)
-            client.recv(silent=True)
+        # Cycle game phases
+        for phase in range(0, 11):
+            cmd_setactivephase = Command_SetActivePhase()
+            cmd_setactivephase.phase = phase
+            client.send_command(cmd_setactivephase)
 
+            # Wait until the phase has been changed
+            client.wait(lambda: client.active_phase == phase)
+
+            if phase == 0:
+                # Untap cards
+                for card in client.table:
+                    if card.tapped:
+                        tap_card(card, False)
+
+            if phase == 2:
+                # Draw card in Draw phase
+                cmd_drawcards = Command_DrawCards()
+                cmd_drawcards.number = 1
+                client.send_command(cmd_drawcards)
+                client.recv()
+
+            if phase == 3:
+                # Play a land in Main phase
+                land = find_land(client.hand)
+                if land:
+                    play_card(land)
+
+                # Try to play a creature
+                creatures = find_creatures(client.hand)
+                for creature in creatures:
+                    lands = find_lands_to_pay_for(creature, client.table)
+                    if lands is not None:
+                        for land in lands:
+                            tap_card(land)
+                        play_card(creature)
+                        break  # Don't play more than one creature
+
+            time.sleep(0.2)
+
+        cmd_nextturn = Command_NextTurn()
+        client.send_command(cmd_nextturn)
+
+        # Wait until it's other player's turn
+        client.wait(lambda: client.active_player_id != client.player_id)
+
+
+    client.wait(lambda: False)
 
